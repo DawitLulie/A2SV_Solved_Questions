553872b9c62e2b866b2d453944b6edc6c832a564
Dawit Lulie <dawitlulie.lulie@aastustudent.edu.et>
2026-02-06 21:36:04 +0300

    ⏱ Time: 784ms (8.48%) | 💾 Memory: 19.86MB (51.19%)

---
+++ 136-Single-Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = Counter(nums)
+        for key, val in dic.items():
+            if val == 1:
+                return key
+        
+        
+          
+
+             
+      
