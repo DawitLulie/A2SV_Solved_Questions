c47e2abc6923813d8e8ef8453999041bb69d1f71
Dawit Lulie <dawitlulie.lulie@aastustudent.edu.et>
2026-02-09 17:06:44 +0300

    ⏱ Time: 330ms (8.53%) | 💾 Memory: 57.82MB (8.40%)

---
+++ 380-Insert-Delete-GetRandom.py
@@ -0,0 +1,22 @@
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        self.myset = set()
+        
+    def insert(self, val: int) -> bool:
+        if val not in self.myset:
+            self.myset.add(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.myset:
+            self.myset.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return random.choice(list(self.myset))
+
+      
