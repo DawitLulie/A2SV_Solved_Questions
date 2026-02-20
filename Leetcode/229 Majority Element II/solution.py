bb88daa8adb77463bebbedda722112eae8e2d3fc
Dawit Lulie <dawitlulie.lulie@aastustudent.edu.et>
2026-02-20 14:18:19 +0300

    ⏱ Time: 91 ms (40.29%) | 💾 Memory: 19.80 MB (28.23%)

--- Leetcode/229 Majority Element II/solution.py
+++ Leetcode/229 Majority Element II/solution.py
@@ -1,13 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        from collections import Counter
-        arr = Counter(nums)
-        n = len(nums)
-        result = []
-        for key, val in arr.items():
-            if val > n / 3:
-                result.append(key)
-        return result
+        counter = Counter(nums)
+        return [num for num in counter if counter[num] > len(nums) // 3]
+        
 
             
 
