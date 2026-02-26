af18ebcedf97490349ff6aa2bf9b71349ad1dfe9
Dawit Lulie <dawitlulie.lulie@aastustudent.edu.et>
2026-02-26 17:31:41 +0300

    ⏱ Time: O(n) | 💾 Memory: O(1)

--- Codeforces/C Number of Equal/solution.py
+++ Codeforces/C Number of Equal/solution.py
@@ -1,36 +1,28 @@
-from collections import Counter
-n, m = list(map(int, input().split()))
+n, m = map(int, input().split())
 arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
- 
+
 i = j = 0
 count = 0
-cur = prev = 0
 
 while i < n and j < m:
-  if i > 0 and arr1[i] == arr1[i-1]:
-    count += prev
-    i += 1
-  
-  elif arr1[i] == arr2[j]:
-    cur += 1
-    j += 1
-
-
-  else:
-    count += cur
-    prev = cur
-    cur = 0
-    
-    if arr1[i] > arr2[j]:
-      j += 1
-      
+    if arr1[i] < arr2[j]:
+        i += 1
+    elif arr1[i] > arr2[j]:
+        j += 1
     else:
-      i += 1
-
-count += cur
+        cur = arr1[i]
+        count1 = 0
+        count2 = 0
+        
+        while i < n and arr1[i] == cur:
+            count1 += 1
+            i += 1
+        
+        while j < m and arr2[j] == cur:
+            count2 += 1
+            j += 1
+        
+        count += (count1 * count2)
 
 print(count)
-
-    
-  
